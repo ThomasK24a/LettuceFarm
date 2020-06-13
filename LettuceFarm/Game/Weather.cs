0a34c47ca2afa2a9f84777c5d5f8fa9f0d2c404e
Ademola <adigun673@gmail.com>
2020-06-13 21:24:39 +0200

    Revert "Fixed the cow,"
    This reverts commit 89098380746eea0d57aa60b3817b673c3d6e18fb.

--- LettuceFarm/Game/Weather.cs
+++ LettuceFarm/Game/Weather.cs
@@ -5,41 +5,41 @@ using System.Text;
 
 namespace LettuceFarm.Game
 {
-    public class Weather
-    {
-        int[] temperature;
-        int[] humidity;
-        int[] sunshine;
-        int value;
+	public class Weather
+	{
+		int[] temperature;
+		int[] humidity;
+		int[] sunshine;
+		int value;
 
-        public Weather()
-        {
-            temperature = new int[] { 20, 25, 30, 35, 40, 50, 55 };
-            humidity = new int[] { 20, 25, 30, 35, 40, 50, 55 };
-            sunshine = new int[] { 20, 25, 30, 35, 40, 50, 55 };
-            value = 0;
-        }
+		public Weather()
+		{
+			temperature = new int[] { 20, 25, 30, 35, 40, 50, 55};
+			humidity = new int[] { 20, 25, 30, 35, 40, 50, 55 };
+			sunshine = new int[] { 20, 25, 30, 35, 40, 50, 55 };
+			value = 0;
+		}
 
-        public int randomTemp()
-        {
-            Random random = new Random();
-            int index = random.Next(0, 7);
+		public int randomTemp()
+		{
+			Random random = new Random();
+			int index = random.Next(0, 7);
 
-            return value + temperature[index];
-        }
+			return value + temperature[index];
+		}
 
-        public int randomHumidity()
-        {
-            Random random = new Random();
-            int index = random.Next(0, 7);
-            return value + humidity[index];
-        }
+		public int randomHumidity()
+		{
+			Random random = new Random();
+			int index = random.Next(0, 7);
+			return value + humidity[index];
+		}
 
-        public int randomSun()
-        {
-            Random random = new Random();
-            int index = random.Next(0, 7);
-            return value + sunshine[index];
-        }
-    }
+		public int randomSun()
+		{
+			Random random = new Random();
+			int index = random.Next(0, 7);
+			return value + sunshine[index];
+		}
+	}
 }
