c0c866542bf056ff3f72fd917689d4932de69e1c
IsaacSem <47461659+IsaacSem@users.noreply.github.com>
2020-06-21 14:09:06 +0200

    Revert "Deleted maptiile.cs"
    This reverts commit cfb58eb5715d2009151964e6ef05605d40f04c8f.

---
+++ LettuceFarm/Game/Map/MapTile.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LettuceFarm
+{
+    class MapTile
+    {
+        private int tileWidth;
+        private int tileHeight;
+        private int width;
+        private int height;
+        //private Texture2D texture;
+
+        public MapTile(int pTileHeight, int pTileWidth, int pWidth, int pHeight)
+        {
+            tileWidth = pTileWidth;
+            tileHeight = pTileHeight;
+            width = pWidth;
+            height = pHeight;
+        }
+
+        public void draw(SpriteBatch spriteBatch)
+        {
+            Vector2 tilePosition = Vector2.Zero;
+        }
+    }
+}
